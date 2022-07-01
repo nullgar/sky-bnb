80188600c8de228b4e9d9744581b0fa6eb9a2d3c
Jon Aguilar <technicianaguilar@gmail.com>
2022-07-01 12:39:50 -0700

    Implemented Demo User Button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import * as sessionActions from '../../store/session'
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
+  const demoUserLogin = (e) => {
+    e.preventDefault();
+    return dispatch(sessionActions.login({ credential: 'nullgar', password: 'testing@1' }))
 
+  }
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
@@ -17,6 +23,7 @@ function Navigation({ isLoaded }){
       <>
         <NavLink to="/login">Log In</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
+        <button onClick={demoUserLogin}>Demo User</button>
       </>
     );
   }
